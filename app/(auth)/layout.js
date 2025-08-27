4348a7d938fc430db1c4a1fbfdbec0f3226f2d2f
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-08-27 19:30:04 +0530

    Created Sign-in

---
+++ app/(auth)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">{children}</div>
+  )
+}
+
+export default AuthLayout;
