106ac48dc7490702550e4aa416b0e0823c25b89d
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-08-27 20:09:26 +0530

    Created Sign-in

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Divide } from "lucide-react";
 
 export default function Home() {
   return (
+    <div className="mt-40">
     <Button variant="destructive">Sneha Kushwaha</Button>
+    </div>
   );
 }
 
--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,36 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
-    <div className='fixed top-0'>
-        <SignedOut>
-              <SignInButton />
-        </SignedOut>
-        <SignedIn>
-              <UserButton/>
-        </SignedIn>
-              
+    <div className=' w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+      <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
+        {/* Logo */}
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt='wisepenny logo'
+            height={60}
+            width={200}
+            className="h-12 w-auto object-contain"
+          />
+        </Link>
+
+        {/* Auth Buttons */}
+        <div>
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/dashboard'>
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
     </div>
   );
 };
